33160083fd33f524b259aea2fdb57ec7024c6579
Paul <comrade_pasha@ukr.net>
2016-12-06 13:56:46 +0200

    v001 swift 2.3

--- web-request-test/ViewController.swift
+++ web-request-test/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import WebKit
+
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView = WKWebView()
+        container.addSubview(webView)
+        
+        //print(container.bounds.height)
+        //print(container.bounds.width)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://developer.apple.com/swift/")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadRequest(urlStr: String) {
 
+        let url = NSURL(string: urlStr)!
+        let urlRequest = NSURLRequest(URL: url)
+        webView.loadRequest(urlRequest)
+    }
+    
+    @IBAction func loadSwift(sender: AnyObject) {
+        loadRequest("https://developer.apple.com/swift/")
+    }
+
+    @IBAction func loadYoutube(sender: AnyObject) {
+        loadRequest("https://www.youtube.com/")
+    }
+    
+    @IBAction func loadParus(sender: AnyObject) {
+        loadRequest("http://parus.besaba.com/")
+    }
 }
 
